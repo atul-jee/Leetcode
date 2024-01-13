92bea76b3daa329547b77fbcbd0deda457e8e6cf
Atul jee <137196654+atul-jee@users.noreply.github.com>
2024-01-13 19:12:56 +0530

    Time: 102 ms (95.08%) | Memory: 17.5 MB (80.89%) - LeetSync

--- 695-max-area-of-island/max-area-of-island.py
+++ 695-max-area-of-island/max-area-of-island.py
@@ -1,24 +1,25 @@
 class Solution:
-
-    def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
+    def helper(self,grid,i,j):
         r=len(grid)
         c=len(grid[0])
-        def dfs(i,j):
-            nonlocal r,c
-            if i<0 or j<0 or i>=r or j>=c or grid[i][j]==0:
-                return 0
-            grid[i][j]=0
-            area=1
-            area+=dfs(i+1,j)
-            area+=dfs(i-1,j)
-            area+=dfs(i,j-1)
-            area+=dfs(i,j+1)
-            return area
+        if i<0 or j<0 or i>=r or j>=c or grid[i][j]==0:
+            return 0
+        grid[i][j]=0
+        area=1
+        area+=self.helper(grid,i-1,j)
+        area+=self.helper(grid,i+1,j)
+        area+=self.helper(grid,i,j+1)
+        area+=self.helper(grid,i,j-1)
+        return area
+
+
+    def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
         ans=0
         for i in range(len(grid)):
             for j in range(len(grid[0])):
-                if grid[i][j]==1:
-                    ans=max(ans,dfs(i,j))
+                if grid[i][j]:
+                    a=self.helper(grid,i,j)
+                    ans=max(ans,a)
         return ans
 
         
