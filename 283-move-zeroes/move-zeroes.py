ca11e6dd77da7adb6ad04749c7bb8f249b875bf0
Atul jee <137196654+atul-jee@users.noreply.github.com>
2024-04-27 11:20:53 +0530

    Time: 121 ms (82.90%) | Memory: 18 MB (78.30%) - LeetSync

--- 283-move-zeroes/move-zeroes.py
+++ 283-move-zeroes/move-zeroes.py
@@ -9,13 +9,13 @@ class Solution:
         i=0
         j=1
         while j<n:
-            if nums[i] == 0 and nums[j] != 0:
-                nums[i], nums[j] = nums[j], nums[i]
-                i += 1
-                j += 1
-            elif nums[i] == 0 and nums[j] == 0:
-                j += 1
+            if nums[i]==0 and nums[j]!=0:
+                nums[i],nums[j]=nums[j],nums[i]
+                i+=1
+                j+=1
+            elif nums[i]==nums[j]==0:
+                j+=1
             else:
-                i += 1
-                j += 1
+                i+=1
+                j+=1
         
