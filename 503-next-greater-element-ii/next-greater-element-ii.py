57db14517c76c859337de7be2774b8a95593d396
Atul jee <137196654+atul-jee@users.noreply.github.com>
2024-05-14 16:34:48 +0530

    Time: 157 ms (40.57%) | Memory: 18.5 MB (32.68%) - LeetSync

--- 503-next-greater-element-ii/next-greater-element-ii.py
+++ 503-next-greater-element-ii/next-greater-element-ii.py
@@ -1,13 +1,18 @@
+from collections import defaultdict
+
 class Solution:
     def nextGreaterElements(self, nums: List[int]) -> List[int]:
         n = len(nums)
         result = [-1] * n
         stack = []
-        
-        for i in range(2 * n):
+        d = defaultdict(int)
+        for i in range(n * 2):
             while stack and nums[stack[-1]] < nums[i % n]:
-                result[stack.pop()] = nums[i % n]
+                d[stack.pop()] = nums[i % n]
             if i < n:
                 stack.append(i)
-        
+
+        for i in range(n):
+            result[i] = d.get(i, -1)
+
         return result
