3f73df072ced309803e27f355af7386f1d024027
Atul jee <137196654+atul-jee@users.noreply.github.com>
2024-07-14 19:04:11 +0530

    Time: 64 ms (75.07%) | Memory: 16.6 MB (57.61%) - LeetSync

--- 733-flood-fill/flood-fill.py
+++ 733-flood-fill/flood-fill.py
@@ -1,17 +1,22 @@
+from typing import List
+
 class Solution:
     def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
-        initialColor=image[sr][sc]
-        def dfs(i,j,row,col,initialColor):
-            nonlocal color
-            if i<0 or i>=row or j<0 or j>=col or initialColor!=image[i][j]:
-                return
-            image[i][j]=color
-            dfs(i+1,j,row,col,initialColor)
-            dfs(i-1,j,row,col,initialColor)
-            dfs(i,j+1,row,col,initialColor)
-            dfs(i,j-1,row,col,initialColor)
-        if image[sr][sc]!=color:
-            dfs(sr,sc,len(image),len(image[0]),initialColor)
-        return image
-
+        if image[sr][sc] == color:
+            return image
         
+        row, col = len(image), len(image[0])
+        initialColor = image[sr][sc]
+        q = [(sr, sc)]
+        dirs = [(1, 0), (-1, 0), (0, -1), (0, 1)]
+        
+        while q:
+            u, v = q.pop(0)
+            image[u][v]=color
+            for i, j in dirs:
+                di, dj = u + i, v + j
+                if 0 <= di < row and 0 <= dj < col and image[di][dj] == initialColor:
+                    image[di][dj] = color
+                    q.append((di, dj))
+        
+        return image
