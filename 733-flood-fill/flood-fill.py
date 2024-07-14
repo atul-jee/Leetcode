2c2e1b59b42eda694c80351e6af42e5e7ef46f0a
Atul jee <137196654+atul-jee@users.noreply.github.com>
2024-07-14 18:55:51 +0530

    Time: 60 ms (90.32%) | Memory: 16.7 MB (57.61%) - LeetSync

--- 733-flood-fill/flood-fill.py
+++ 733-flood-fill/flood-fill.py
@@ -1,19 +1,17 @@
 class Solution:
     def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
-        r=len(image)
-        c=len(image[0])
-        def dfs(i,j,initialColor,newColor):
-            nonlocal r,c
-            if i<0 or j<0 or i>=r or j>=c or image[i][j]!=initialColor:
-                return
-            image[i][j]=newColor
-            dfs(i-1,j,initialColor,newColor)
-            dfs(i+1,j,initialColor,newColor)
-            dfs(i,j-1,initialColor,newColor)
-            dfs(i,j+1,initialColor,newColor)
         initialColor=image[sr][sc]
-        if initialColor!=color:
-            dfs(sr,sc,initialColor,color)
+        def dfs(i,j,row,col,initialColor):
+            nonlocal color
+            if i<0 or i>=row or j<0 or j>=col or initialColor!=image[i][j]:
+                return
+            image[i][j]=color
+            dfs(i+1,j,row,col,initialColor)
+            dfs(i-1,j,row,col,initialColor)
+            dfs(i,j+1,row,col,initialColor)
+            dfs(i,j-1,row,col,initialColor)
+        if image[sr][sc]!=color:
+            dfs(sr,sc,len(image),len(image[0]),initialColor)
         return image
 
-
+        
