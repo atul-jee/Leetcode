6f847ce8b2ea411cf9e7a8d1ca9c933034815de3
Atul jee <137196654+atul-jee@users.noreply.github.com>
2024-08-01 12:21:23 +0530

    Time: 433 ms (95.24%) | Memory: 27.8 MB (57.78%) - LeetSync

--- 1073-number-of-enclaves/number-of-enclaves.py
+++ 1073-number-of-enclaves/number-of-enclaves.py
@@ -4,21 +4,27 @@ class Solution:
         def dfs(i,j):
             if i<0 or j<0 or i>=m or j>=n or grid[i][j]!=1:
                 return 
-            grid[i][j]=0
+            grid[i][j]=2
             dfs(i+1,j)
-            dfs(i-1,j)
-            dfs(i,j-1)
             dfs(i,j+1)
+            dfs(i,j-1)
+            dfs(i-1,j)
         for i in range(m):
-            dfs(i,0)
-            dfs(i,n-1)
-        for j in range(n):
-            dfs(0,j)
-            dfs(m-1,j)
+            if grid[i][0]==1:
+                dfs(i,0)
+            if grid[i][n-1]==1:
+                dfs(i,n-1)
+        for i in range(n):
+            if grid[0][i]==1:
+                dfs(0,i)
+            if grid[m-1][i]==1:
+                dfs(m-1,i)
         count=0
         for i in range(m):
             for j in range(n):
-                if grid[i][j]:
+                if grid[i][j]==1:
                     count+=1
-                    #dfs(i,j)
+            
         return count
+
+        
